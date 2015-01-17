55ae0e698c313b3b4824041f3952a08ff06083d4
unknown <nkukret@IN-GUR-NKUKRET.comverse.com>
2015-01-17 22:32:49 +0530

    Init workspace

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shareRideAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shareRideAppApp
+ */
+angular.module('shareRideAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
